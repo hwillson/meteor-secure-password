c3456aa8cf0bdea4cb8cdce03092bbab1e185e20
hwillson <hugh@octonary.com>
2015-02-25 06:35:48 -0500

    Adjusted password generation to use uppercase characters as well.

--- hwillson:secure-password-tests.js
+++ hwillson:secure-password-tests.js
@@ -1,23 +1,23 @@
 Tinytest.add('Generates a random password', function (test) {
 
   test.isNotNull(
-    SecurePassword.generate(),
+    SecurePassword.generatePassword(),
     'Generate password should not be null'
   );
 
   test.length(
-    SecurePassword.generate(),
+    SecurePassword.generatePassword(),
     15,
     'Generated password should be 15 characters long'
   );
 
   test.isTrue(
-    SecurePassword.generate().match(/[a-z0-9]{3}/g).length === 4,
+    SecurePassword.generatePassword().match(/[a-zA-Z0-9]{3}/g).length === 4,
     'Generate password should match the format xxx-xxx-xxx-xxx'
   );
 
-  var password1 = SecurePassword.generate();
-  var password2 = SecurePassword.generate();
+  var password1 = SecurePassword.generatePassword();
+  var password2 = SecurePassword.generatePassword();
   test.notEqual(
     password1,
     password2,
--- hwillson:secure-password.js
+++ hwillson:secure-password.js
@@ -1,14 +1,38 @@
-SecurePassword = {
+SecurePassword = (function () {
+
+	// Random length of string characters used to generate the new password.
+	var randomStringLength = 12;
+
+	// Allowed password characters.
+	var allowedChars =
+		'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 	/**
 	 * Generate a secure random password.
 	 *
 	 * @return  {String}  Random password using the format xxx-xxx-xxx-xxx.
 	 */
-	generate: function () {
-		var primer = Math.random().toString(36).slice(2, 14);
-		var random = primer.match(/.../g).join('-');
-		return random;
-	}
+	var generatePassword = function () {
+		return randomString().match(/.../g).join('-');
+	};
 
-};
+	/**
+	 * Generate a random string used to build the new password.
+	 *
+	 * @return  {String}  Random string with length randomStringLength, built
+	 *                    using allowedChars array.
+	 */
+	var randomString = function () {
+		var randomString = '', i;
+    for (i = 0; i < randomStringLength; i += 1) {
+			randomString +=
+				allowedChars[Math.round(Math.random() * (allowedChars.length - 1))];
+		}
+    return randomString;
+	};
+
+	return {
+		generatePassword: generatePassword
+	};
+
+}());
